e3d03e4a5904b769c4e45d802a59017238b772c9
Paul <paul.bartholomew08@imperial.ac.uk>
2018-10-03 14:04:48 +0100

    Setting .dir-locals.el to agree with python defaults

---
+++ .dir-locals.el
@@ -0,0 +1,37 @@
+;;; .dir-locals.el --- 
+;; 
+;; Filename: .dir-locals.el
+;; Description: 
+;; Author: Paul Bartholomew
+;; Maintainer: 
+;; Created: Wed Oct  3 14:03:18 2018 (+0100)
+;; Version: 
+;; Package-Requires: ()
+;; Last-Updated: Wed Oct  3 14:04:42 2018 (+0100)
+;;           By: Paul Bartholomew
+;;     Update #: 2
+;; URL: 
+;; Doc URL: 
+;; Keywords: 
+;; Compatibility: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+((nil . ((indent-tabs-mode . nil)
+				 (python-indent-offset . 4))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; .dir-locals.el ends here
