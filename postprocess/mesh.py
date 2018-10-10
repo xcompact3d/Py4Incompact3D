9f28838246fe805d4f114ff0105360d1a5d68eb8
GD_mac <gdeskosv@gmail.com>
2018-10-10 14:22:36 +0100

    In compute_derivvars -- correcting boundary conditions

--- postprocess/mesh.py
+++ postprocess/mesh.py
@@ -41,10 +41,18 @@ class Mesh():
 
     def compute_derivvars(self):
         """ Compute variables required by derivative functions. """
-
-        self.dx = self.Lx / float(self.Nx)
-        self.dy = self.Ly / float(self.Ny) # XXX This will not be correct for stretched grids
-        self.dz = self.Lz / float(self.Nz)
+	if (self.BCx==0):
+        	self.dx = self.Lx / float(self.Nx)
+        else:
+        	self.dx = self.Lx / float(self.Nx-1)
+	if (self.BCy==0):	
+		self.dy = self.Ly / float(self.Ny) # XXX This will not be correct for stretched grids
+        else:
+		self.dy = self.Ly / float(self.Ny-1) # XXX This will not be correct for stretched grids
+	if (self.BCz==0):	
+        	self.dz = self.Lz / float(self.Nz)
+        else:
+		self.dz=self.Lz/float(self.Nz-1)
 
         self.alpha = 1.0 / 3.0
         self.a = 14.0 / 9.0
