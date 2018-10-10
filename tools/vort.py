fc1e4fe452a0e75007f4c5f5095972fa70f2d213
Paul <paul.bartholomew08@imperial.ac.uk>
2018-10-10 11:57:50 +0100

    Finishing vorticity calculator

--- tools/vort.py
+++ tools/vort.py
@@ -6,6 +6,7 @@
 """
 
 from Py4Incompact3D.deriv.deriv import deriv
+from Py4Incompact3D.postprocess.fields import Field
 
 def calc_vort(postprocess, time=-1):
     """ Computes the vorticity of the velocity field, assumes ux uy and uz have all been loaded. 
@@ -38,4 +39,29 @@ def calc_vort(postprocess, time=-1):
                 S[i][j] = deriv(postprocess, vel, j, t)
         
         # Compute vorticity field
+        vortx = S[2][1] - S[1][2]
+        vorty = S[0][2] - S[2][0]
+        vortz = S[1][0] - S[0][1]
+
+        prop_dict = {"name":"vortx",
+                     "description":"x-component of vorticity",
+                     "properties":{"filename":"vortx",
+                                   "direction":[0],
+                                   "precision":postprocess.fields["ux"].dtype}}
+        postprocess.fields["vortx"] = Field(prop_dict)
+        postprocess.fields["vortx"].data[t] = vortx
+        prop_dict = {"name":"vorty",
+                     "description":"y-component of vorticity",
+                     "properties":{"filename":"vorty",
+                                   "direction":[1],
+                                   "precision":postprocess.fields["uy"].dtype}}
+        postprocess.fields["vorty"] = Field(prop_dict)
+        postprocess.fields["vorty"].data[t] = vorty
+        prop_dict = {"name":"vortz",
+                     "description":"z-component of vorticity",
+                     "properties":{"filename":"vortz",
+                                   "direction":[2],
+                                   "precision":postprocess.fields["uz"].dtype}}
+        postprocess.fields["vortz"] = Field(prop_dict)
+        postprocess.fields["vortz"].data[t] = vortz
 
