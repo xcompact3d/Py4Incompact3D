c9f8c37972b2c71881739fc6a388265887b7a76f
Paul <paul.bartholomew08@imperial.ac.uk>
2018-10-19 11:02:22 +0100

    Adding helper tools int_over_axis and avg_over_axis
    These return the integral/average of an array over an axis
    
    Also get_frontidx_birman to find the front locations in lockexchange

--- examples/lockexch/plot_h.py
+++ examples/lockexch/plot_h.py
@@ -15,7 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from Py4Incompact3D.postprocess.postprocess import Postprocess
-from Py4Incompact3D.tools.lockexch import calc_h
+from Py4Incompact3D.tools.lockexch import calc_h, get_frontidx_birman
 from Py4Incompact3D.tools.misc import moving_avg
 
 ####################################################################################################
@@ -65,15 +65,16 @@ def main():
                   postprocess.mesh.Lx)
 
     ## Loop over time, compute h and plot
+    h = {}
     for t in range(T):
 
         # Get h
         postprocess.load(time=t)
-        h = calc_h(postprocess)
+        h[t] = calc_h(postprocess)[t]
         postprocess.clear_data()
 
         # Smooth h using moving average
-        h[t] = np.array(moving_avg(h[t], NSMPL))
+        h[t] = np.array(moving_avg(h[t], 0, NSMPL))
 
         # Plot
         plt.figure(figsize=(PLTX, PLTY))
@@ -83,6 +84,43 @@ def main():
         filename = NAMERT + "-t" + str(t) + FIGTYPE
         plt.savefig(filename, bbox_inches="tight")
         plt.close()
+
+    ## Get fronts and compute velocity
+    idxr, idxw, idxf = get_frontidx_birman(h)
+    ur, uf = [], []
+    n = len(h.keys())
+    keys_ordered = sorted(h,keys())
+    for t in range(1, n):
+        idxr0 = idxr[keys_ordered[t - 1]]
+        if idxr0:
+            xr0 = idxr0 * postprocess.mesh.dx
+        else:
+            xr0 = 0
+
+        idxrn = idxr[keys_ordered[t]]
+        if idxrn:
+            xrn = idxrn * postprocess.mesh.dx
+        else:
+            xrn = 0
+
+        ur.append((xrn - xr0) / float(keys_ordered[t] - keys_ordered[t - 1]))
+
+        idxf0 = idxf[keys_ordered[t - 1]]
+        if idxf0:
+            xf0 = idxf0 * postprocess.mesh.dx
+        else:
+            xf0 = 0
+
+        idxfn = idxf[keys_ordered[t]]
+        if idxfn:
+            xfn = idxfn * postprocess.mesh.dx
+        else:
+            xfn = 0
+
+        uf.append((xfn - xf0) / float(keys_ordered[t] - keys_ordered[t - 1]))
+    plt.plot(ur)
+    plt.plot(uf)
+    plt.show()
 
 ####################################################################################################
 #
