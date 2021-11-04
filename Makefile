68b1eddb39ec07c31362aa944cfec98500976238
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2021-11-04 21:18:21 +0000

    Beginning effort to wrap 2decomp&fft with f2py

--- Makefile
+++ Makefile
@@ -4,6 +4,35 @@
 # DESCRIPTION: Makefile for Py4Incompact3D: runs tests and builds documentation.
 #
 
+DEFS = -DDOUBLE_PREC
+
+FC = mpif90
+FFLAGS = -cpp -O3 -funroll-loops -floop-optimize -g -Warray-bounds -fcray-pointer -fbacktrace -ffree-line-length-none
+F2PY = python3 -m numpy.f2py --f90exec=$(FC)
+
+OBJ_DIR = obj
+
+SRC = ./py4incompact3d.f90
+
+DEP = $(D2DIR)/decomp_2d.f90
+OBJ := $(DEP:%.f90=%.o)
+OBJ := $(patsubst $(D2DIR)/%.o,%.o,$(OBJ))
+OBJ := $(addprefix $(OBJ_DIR)/,$(OBJ))
+
+
+decomp2d: $(OBJ_DIR) $(SRC) $(OBJ)
+	$(info $(OBJ))
+	$(F2PY) --f90flags="$(FFLAGS) $(DEFS)" -I$(OBJ_DIR) -m $@ -c $(filter-out $(OBJ_DIR),$^)
+
+$(OBJ_DIR)/%.o: %.f90
+	$(FC) $(FFLAGS) -J$(OBJ_DIR) -c $^ -o $@
+
+$(OBJ_DIR)/%.o: $(D2DIR)/%.f90
+	$(FC) $(FFLAGS) -J$(OBJ_DIR) -c $^ -o $@
+
+$(OBJ_DIR):
+	mkdir $(OBJ_DIR)
+
 all: test
 
 test:
@@ -13,3 +42,7 @@ test:
 doc:
 	make -C doc/ latexpdf
 
+
+.PHONY: clean
+clean:
+	rm -f *.mod *.o *.so
---
+++ py4incompact3d.f90
@@ -0,0 +1,41 @@
+!!! py4incompact3d.f90 provides light-weight interface to
+!!!                    2decomp&fft/Xcompact3d for wrapping with f2py.
+!!! Copyright (C) 2021  University of Edinburgh
+!!!
+!!! This program is free software: you can redistribute it and/or modify
+!!! it under the terms of the GNU General Public License as published by
+!!! the Free Software Foundation, either version 3 of the License, or
+!!! (at your option) any later version.
+!!!
+!!! This program is distributed in the hope that it will be useful,
+!!! but WITHOUT ANY WARRANTY; without even the implied warranty of
+!!! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!!! GNU General Public License for more details.
+!!!
+!!! You should have received a copy of the GNU General Public License
+!!! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+module py4incompact3d
+  ! A module to wrap the 2decomp&fft library such that it can be wrapped by f2py.
+  
+  use decomp_2d
+  
+  implicit none
+
+  private
+  public :: init_py4incompact3d
+
+contains
+
+  subroutine init_py4incompact3d(nx, ny, nz, p_row, p_col)
+    ! Initialises the 2decomp&fft library for a domain discretised by :math:`n_x \times n_y \times
+    ! n_z` mesh points with a :math:`p_{row} \times p_{col}` pencil decomposition.
+    
+    integer, intent(in) :: nx, ny, nz
+    integer, intent(in) :: p_row, p_col
+
+    call decomp_2d_init(nx, ny, nz, p_row, p_col)
+    
+  end subroutine init_py4incompact3d
+  
+end module py4incompact3d
