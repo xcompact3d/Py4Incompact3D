e1dc6b3d28a9f90c70a50a35d6777e58cb5aa6f8
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2022-04-26 10:18:33 +0100

    Update transpose_glbaol_array example/test

--- examples/parallel/transpose_global_arr.py
+++ examples/parallel/transpose_global_arr.py
@@ -9,16 +9,17 @@
 """
 
 import py4incompact3d
+from py4incompact3d import parallel
 
-N=64
+N=2
 L=1.0
 BC=0
 
 def init():
 
     nx = N
-    ny = N
-    nz = N
+    ny = 2 * N
+    nz = 4 * N
     n = [nx, ny, nz]
 
     l = [L, L, L]
@@ -29,11 +30,26 @@ def init():
         n = n, l = l, bc = bc
     )
 
-    return mesh
+    phiX = py4incompact3d.postprocess.Field()
+    phiY = py4incompact3d.postprocess.Field()
+    phiZ = py4incompact3d.postprocess.Field()
+    phiX.new(mesh, pencil=0)
+    phiY.new(mesh, pencil=1)
+    phiZ.new(mesh, pencil=2)
+    
+    return mesh, phiX, phiY, phiZ
 
 def main():
 
-    mesh = init()
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+
+    parallel.print_par("="*72)
+    parallel.print_par("Running transpose example")
+    parallel.print_par("-"*72)
+    
+    mesh, phiX, phiY, phiZ = init()
 
     # Pre-transpose
     pencil = 0
@@ -42,43 +58,61 @@ def main():
             for k in range(mesh.NzLocal[pencil]):
 
                 idxg = mesh.global_index(pencil, i, j, k)
-                # TODO: Set field value
+                phiX.data[0][i][j][k] = idxg
                 
     # Transpose X->Y
+    parallel.print_par("Testing X-Y transpose")
     pencil = 1
+    phiY.data[0] = parallel.transpose(phiX.data[0], "xy", phiY.data[0])
     for i in range(mesh.NxLocal[pencil]):
         for j in range(mesh.NyLocal[pencil]):
             for k in range(mesh.NzLocal[pencil]):
 
                 idxg = mesh.global_index(pencil, i, j, k)
-                # TODO: Check field value
-
+                assert(phiY.data[0][i][j][k] == idxg)
+    comm.Barrier()
+    parallel.print_par("+ PASSED")
+    
     # Transpose Y->Z
+    parallel.print_par("Testing Y-Z transpose")
     pencil = 2
+    phiZ.data[0] = parallel.transpose(phiY.data[0], "yz", phiZ.data[0])
     for i in range(mesh.NxLocal[pencil]):
         for j in range(mesh.NyLocal[pencil]):
             for k in range(mesh.NzLocal[pencil]):
 
                 idxg = mesh.global_index(pencil, i, j, k)
-                # TODO: Check field value
+                assert(phiZ.data[0][i][j][k] == idxg)
+    comm.Barrier()
+    parallel.print_par("+ PASSED")
                 
     # Transpose Z->Y
+    parallel.print_par("Testing Z-Y transpose")
     pencil = 1
+    phiY.data[0][:][:][:] = 0 # Explicitly zero to ensure data transposed back
+    phiY.data[0] = parallel.transpose(phiZ.data[0], "zy", phiY.data[0])
     for i in range(mesh.NxLocal[pencil]):
         for j in range(mesh.NyLocal[pencil]):
             for k in range(mesh.NzLocal[pencil]):
 
                 idxg = mesh.global_index(pencil, i, j, k)
-                # TODO: Check field value
+                assert(phiY.data[0][i][j][k] == idxg)
+    comm.Barrier()
+    parallel.print_par("+ PASSED")
                 
     # Transpose Y->X
+    parallel.print_par("Testing Y-X transpose")
     pencil = 0
+    phiX.data[0][:][:][:] = 0 # Explicitly zero to ensure data transposed back
+    phiX.data[0] = parallel.transpose(phiY.data[0], "yx", phiX.data[0])
     for i in range(mesh.NxLocal[pencil]):
         for j in range(mesh.NyLocal[pencil]):
             for k in range(mesh.NzLocal[pencil]):
 
                 idxg = mesh.global_index(pencil, i, j, k)
-                # TODO: Check field value
-                
+                assert(phiX.data[0][i][j][k] == idxg)
+    comm.Barrier()
+    parallel.print_par("+ PASSED")
+
 if __name__ == "__main__":
     main()
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     name = "py4incompact3d",
     author = "Xcompact3d project",
     packages = ["py4incompact3d",
+                "py4incompact3d.parallel",
                 "py4incompact3d.postprocess",
                 "py4incompact3d.tools",
                 "py4incompact3d.deriv"],
