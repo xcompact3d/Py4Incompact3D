d9930a18ad975c32eb59f4ea289cec58df29c8d7
Paul <ptb0890@gmail.com>
2022-04-28 16:21:24 +0100

    Demonstrating parallel reading and processing with mixing layer
    The axial integration tool now works on the local arrays, transposing
    in parallel as necessary and the mixing layer now demonstrates how
    to construct a plane in parallel (this should be abstracted)

--- examples/mixing-layer/mixing-layer.py
+++ examples/mixing-layer/mixing-layer.py
@@ -5,12 +5,14 @@ Reference data available from:
 https://zenodo.org/record/2577851/files/mixing-layer.tar.xz?download=1
 """
 
+from mpi4py import MPI
+
 import numpy as np
 
 import matplotlib as mpl
 # mpl.use('Agg')
 import matplotlib.pyplot as plt
-plt.rc("text", usetex=True)
+#plt.rc("text", usetex=True)
 plt.rc("font", family="serif")
 plt.rc("font", size=11)
 plt.rc("axes", axisbelow=True)
@@ -30,6 +32,9 @@ CONTOURS[-1] *= 0.99
 
 def main():
 
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    
     postprocess = Postprocess('mixing-layer.json')
     
     for t in T:
@@ -41,11 +46,26 @@ def main():
         rho = postprocess.fields["rho"].data[t]
         rho = avg_over_axis(postprocess.mesh, rho, 2)
 
-        plt.figure(figsize=(5, 3.5))
-        plt.contour(rho.transpose(), CONTOURS, colors="black")
-        plt.contour(rhof.transpose(), CONTOURS, colors="red", linestyles="dashed")
-        plt.show()
-        plt.close()
+        # Extract planar data
+        rho_plane = np.zeros((postprocess.mesh.Nx, postprocess.mesh.Ny))
+        if (postprocess.mesh.NzStart[0] == 0):
+            for i in range(postprocess.mesh.NxLocal[0]):
+                for j in range(postprocess.mesh.NyLocal[0]):
+
+                    ii = i + postprocess.mesh.NxStart[0]
+                    jj = j + postprocess.mesh.NyStart[0]
+
+                    rho_plane[ii][jj] = rho[i][j][0]
+
+        rho_plane_out = np.zeros(rho_plane.shape)
+        comm.Reduce(rho_plane, rho_plane_out, op=MPI.SUM)
+
+        if (rank == 0):
+            plt.figure(figsize=(5, 3.5))
+            plt.contour(rho_plane_out.transpose(), CONTOURS, colors="black")
+            #plt.contour(rhof.transpose(), CONTOURS, colors="red", linestyles="dashed")
+            plt.savefig("foo.png")
+            plt.close()
 
         postprocess.clear_data()
 
--- py4incompact3d/tools/misc.py
+++ py4incompact3d/tools/misc.py
@@ -73,26 +73,78 @@ def int_over_axis(mesh, x, axis):
     :returns: xavg - the rank-1 field (the integrated axis is collapsed).
     :rtype: numpy.ndarray
     """
-    if 0 == axis:
-        n = mesh.Nx
-        dx = mesh.dx * np.ones(n)
-        if 0 != mesh.BCx:
-            dx[0] *= 0.5
-            dx[-1] *= 0.5
-    elif 1 == axis:
-        n = mesh.Ny
-        dx = mesh.dy * np.ones(n)
-        if 0 != mesh.BCy:
-            dx[0] *= 0.5
-            dx[-1] *= 0.5
-    else:
-        n = mesh.Nz
-        dx = mesh.dz * np.ones(n)
-        if 0 != mesh.BCz:
-            dx[0] *= 0.5
-            dx[-1] *= 0.5
+    import py4incompact3d
+    from py4incompact3d import parallel
 
-    lastaxis = len(x.shape) - 1
-    xint = dx * np.swapaxes(x, axis, lastaxis)
-    return np.swapaxes(xint, axis, lastaxis).sum(axis=axis)
+    # First transpose data
+    if axis > 0:
+        # Transpose to Y
+        phiX = py4incompact3d.postprocess.Field()
+        phiX.new(mesh, pencil=0)
+        phiX.data[0][:][:][:] = x[:][:][:]
+
+        phiY = py4incompact3d.postprocess.Field()
+        phiY.new(mesh, pencil=1)
+        phiY.data[0] = parallel.transpose(phiX.data[0], "xy", phiY.data[0])
+
+        if axis > 1:
+            # Transpose to Z
+            phiZ = py4incompact3d.postprocess.Field()
+            phiZ.new(mesh, pencil=2)
+            phiZ.data[0] = parallel.transpose(phiY.data[0], "yz", phiZ.data[0])
+
+            phi = phiZ.data[0]
+
+        else:
+            phi = phiY.data[0]
+        
+    else:
+        phi = x
+
+    n = phi.shape[axis]
+    if 0 == axis:
+        dx = mesh.dx
+        bc = mesh.BCx
+    elif 1 == axis:
+        dx = mesh.dy
+        bc = mesh.BCy
+    else:
+        dx = mesh.dz
+        bc = mesh.BCz
+
+    dx = dx * np.ones(n)
+    if bc != 0:
+        dx[0] *= 0.5
+        dx[-1] *= 0.5
+
+    lastaxis = len(phi.shape) - 1
+    phi_int = np.swapaxes(dx * np.swapaxes(phi, axis, lastaxis),
+                       axis,
+                       lastaxis)
+    s = phi_int.sum(axis=axis)
+
+    if axis > 0:
+        if axis > 1:
+            # Transpose from Z
+            for i in range(s.shape[0]):
+                for j in range(s.shape[1]):
+                    phiZ.data[0][i][j][:] = s[i][j]
+
+            phiY.data[0] = parallel.transpose(phiZ.data[0], "zy", phiY.data[0])
+
+        else:
+            for i in range(s.shape[0]):
+                for k in range(s.shape[1]):
+                    phiY.data[0][i][:][k] = s[j][k]
+
+        # Transpose from Y
+        phiX.data[0] = parallel.transpose(phiY.data[0], "yx", phiX.data[0])
+        phi = phiX.data[0]
+
+    else:
+        for j in range(s.shape[0]):
+            for k in range(s.shape[1]):
+                phi[:][j][k] = s[j][k]
+    
+    return phi
 
