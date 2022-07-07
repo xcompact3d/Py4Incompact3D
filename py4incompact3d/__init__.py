1ea5affb2e73dd79e2c91cdc31f1e37c20ae9a69
Paul Bartholomew <p.bartholomew@epcc.ed.ac.uk>
2022-07-07 20:45:08 +0100

    Tidy up how extensions are discovered
    With the old approach it was quite easy to create circular dependencies

--- py4incompact3d/__init__.py
+++ py4incompact3d/__init__.py
@@ -15,27 +15,11 @@ FILE: __init__.py
 
 from mpi4py import MPI
 
-# Try to import h5py for use with HDF5 files
-HAVE_HDF5=False
-try:
-    import h5py
-except ImportError:
-    pass
-else:
-    HAVE_HDF5 = True
+from .extras import *
 
-HAVE_DECOMP2D=False
-try:
-    import decomp2d
-except ImportError:
-    pass
-else:
-    HAVE_DECOMP2D = True
-
-if HAVE_DECOMP2D:
-    HAVE_ADIOS2 = bool(decomp2d.decomp4py.have_adios2)
-else:
-    HAVE_ADIOS2 = False
+from .postprocess import *
+from .tools import *
+from .deriv import *
     
 # Set MPI variables
 comm = MPI.COMM_WORLD
